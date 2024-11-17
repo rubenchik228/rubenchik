335c6f757a93627fbea64c170dbb75d539b35bd7
rubenchik228 <rubenasparan@gmail.com>
2024-11-17 18:18:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -84,13 +84,3 @@ btn6.addEventListener("click", function(){
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
 });
-
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
-usercard.appendChild(p);
